ae6fc650f349fd5852877387a75972cc5582e0f2
inagaki429 <inagaki@ibis-japan.co.jp>
2026-06-15 16:16:08 +0900

    first commit
    6/15

---
+++ src/main/java/in/tech_camp/chatapp_second/ChatappSecondApplication.java
@@ -0,0 +1,13 @@
+package in.tech_camp.chatapp_second;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappSecondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/tech_camp/chatapp_second/ChatappSecondApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.chatapp_second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappSecondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
